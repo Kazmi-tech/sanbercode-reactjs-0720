95826e4dc233b1bcdb92dc9c2afdcff8588ce527
Kazxsit <kazxsit@gmail.com>
2020-07-31 20:56:37 +0700

    menyelesaikan tugas 9

--- Pekan-2/Tugas-Hari-Ke-4/tugas9.js
+++ Pekan-2/Tugas-Hari-Ke-4/tugas9.js
@@ -1,14 +1,52 @@
 // Soal 1
-const newFunction = function literal(firstName, lastName){
+// const newFunction = function literal(firstName, lastName){
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//     fullName: function(){
+//       console.log(firstName + " " + lastName)
+//       return 
+//     }
+//   }
+// }
+ 
+
+const newFunction = (firstName, lastName) => {
   return {
     firstName: firstName,
     lastName: lastName,
-    fullName: function(){
+    fullName: () => {
       console.log(firstName + " " + lastName)
       return 
     }
+
   }
 }
- 
 //Driver Code 
 newFunction("William", "Imoh").fullName() 
+
+
+// Soal 2
+const newObject = {
+  firstName: "Harry",
+  lastName: "Potter Holt",
+  destination: "Hogwarts React Conf",
+  occupation: "Deve-wizard Avocado",
+  spell: "Vimulus Renderus!!!"
+}
+
+const  {firstName, lastName, destination, occupation} = newObject
+
+
+// Driver code
+console.log(firstName, lastName, destination, occupation)
+
+
+
+// Soal 3
+const west = ["Will", "Chris", "Sam", "Holly"]
+const east = ["Gill", "Brian", "Noel", "Maggie"]
+const combined = [...west, ...east]
+
+//Driver Code
+console.log(combined)
